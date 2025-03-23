62b81c69448bf63e972299d1704217914a618214
Miebi <User@DESKTOP-1EK6ASL>
2025-03-23 23:46:37 +0100

    Chore: Created form to create new user role in user management module

---
+++ InventorySystemWebUI/Pages/Module/UserManagement/CreateRole.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InventorySystemWebUI.Pages.Module.UserManagement
+{
+    public class CreateRoleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
