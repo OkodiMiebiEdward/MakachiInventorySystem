4fe1d2b5d39cfc6752021d5450502e2ff126dd19
Miebi <User@DESKTOP-1EK6ASL>
2025-04-17 00:13:17 +0100

    new update

---
+++ InventorySystemWebUI/Pages/Module/UserManagement/CreateUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InventorySystemWebUI.Pages.Module.UserManagement
+{
+    public class CreateUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ InventorySystemWebUI/Pages/Module/UserManagement/RoleList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InventorySystemWebUI.Pages.Module.UserManagement
+{
+    public class RoleListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
